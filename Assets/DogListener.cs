fb2184043a112e2a9263701d32db20d5a24615bb
David Jacka <ahoy.dave@gmail.com>
2017-08-05 11:41:30 +0200

    First commit. Nothing really works

---
+++ Assets/DogListener.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using System.IO.Port;
+
+public class DogListener : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+//		SerialPort sp = new SerialPort ("COM3", 9600);
+//		sp.ReadTimeout = 1;
+//		sp.Open ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
---
+++ Assets/SelectOnInput.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO.Ports;
+
+
+public class SelectOnInput : MonoBehaviour {
+
+	public GameObject PlayerObject;
+
+	private TwineTextPlayer TwinePlayer;
+
+	// Use this for initialization
+	SerialPort sp ;//= new SerialPort("COM13",9600);
+
+	void Start () {
+
+		TwinePlayer = PlayerObject.GetComponent<TwineTextPlayer> ();
+		print (TwinePlayer);
+//		var ports = SerialPort.GetPortNames ();
+//		foreach (var s in ports)
+//			Debug.Log (s);
+//		
+//		Debug.Log ("Hi Guys");
+//		int i = 0;
+//		for (; i < 1; i++)
+//		{        
+//			try {
+//				//print ("HELLO");
+//				print ("/dev/ttyUS" + i.ToString("X"));
+//				sp = new SerialPort("/dev/tty.Bluetooth-Incoming-Port" ,9600);
+//				sp.Open ();
+//				sp.ReadTimeout = 1;
+//				print ("Made it");
+//				break;
+//
+//			} catch (System.Exception e)  {
+//				print (e);
+//			}
+//		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			TwinePlayer.DoChangeSelected ();
+		}
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			TwinePlayer.DoSelect ();
+		}
+			
+//		try {
+//			print (sp.ReadLine());
+//		} catch (System.Exception e)  {
+//			print ("IOException source: {0}");  
+//		}
+	}
+
+}
